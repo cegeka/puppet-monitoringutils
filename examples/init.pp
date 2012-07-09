067841eb56a49662839d7a408c2f66720b8018ed
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-07-09 14:57:01 +0200

    Refactored out scripts dir to separate module + changed example

--- examples/init.pp
+++ examples/init.pp
@@ -1,6 +1,7 @@
-include monitoringutils
-
+include stdlib
 File <| title == '/usr/local/scripts' |>
+
+include monitoringutils
 File <| title == 'fsck-ro.sh' |>
 File <| title == 'fsck-ro.rb' |>
 File <| title == 'appserver-threads-mon.sh' |>
