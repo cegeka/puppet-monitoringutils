91e8fb38eb061980d07f7b0700abd6f3ca73ab3a
Kristof Willaert <kristof.willaert@gmail.com>
2012-07-12 11:45:21 +0200

    Use abort shorthand to fail on errors.

--- files/fsck-ro.rb
+++ files/fsck-ro.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/ruby
 
 # Local variables
-error = 0
 failed_mountpoints = []
 
 # Check for root priv blkid won't run without
-uid = Process.uid
-if uid != 0
-  puts "Sorry: Root must run this script"
-  exit 1
-end
+abort "Sorry: root must run this script" if Process.uid != 0
 
 # Get EXT devices
 blkid = %x[blkid -o device]
@@ -21,16 +16,10 @@ blkid.each { |device|
       return_value = %x[touch -a #{mountpoint}]
       result = $?.to_i
       if result > 0
-        error = 1
         failed_mountpoints << mountpoint
       end
     end
   end
 }
 
-if error >= 1
-  puts "Filesystem readonly-health tests FAILED for the following filesystems: #{failed_mountpoints.join(' ')}"
-  exit 1
-else
-  exit 0
-end
+abort "Filesystem readonly healthcheck FAILED for the following filesystems: #{failed_mountpoints.join(' ')}" if ! failed_mountpoints.empty?
