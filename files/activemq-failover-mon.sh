3b79bb7ce78a5fd93c11047976685f058489500b
Leonardos Charikiopoulos <leonardos.charikiopoulos@cegeka.com>
2025-08-18 16:06:20 +0300

    added -q journalctl option for clearer output

--- files/activemq-failover-mon.sh
+++ files/activemq-failover-mon.sh
@@ -2,6 +2,6 @@
 # Filename: activemq-failover-mon.sh
 
 CURRENT_TIMESTAMP=`date +"%y-%m-%dT%H:%M:%S"`
-TODAYS_ACTIVEMQ_ERRORS=`journalctl -u activemq --since today --grep 'no longer able to keep the exclusive lock so giving up being a master'`
+TODAYS_ACTIVEMQ_ERRORS=`journalctl -u activemq -q --since today --grep 'no longer able to keep the exclusive lock so giving up being a master'`
 
 echo "${CURRENT_TIMESTAMP} - ${TODAYS_ACTIVEMQ_ERRORS}" >> /var/log/activemq-failover-mon.log
